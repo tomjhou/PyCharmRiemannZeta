Fixed sign error in riemann vertical computation. gamma heatmap now works for very high values

plot_critical_line_values.py
@@ -11,23 +11,27 @@ import riemann_math as rm
 if __name__ == "__main__":
     print("Done importing")
 
-height = 8000
-offset = 5
-gap = 0.1
+imag_start = 1
+imag_end = 15000
+imag_gap = 0.01
 
-if rm.RIEMANN_ITER_LIMIT < height * 1.5:
-    rm.RIEMANN_ITER_LIMIT = height * 1.5
+real_offset = 5
+
+iter_limit = imag_end + 2000
+
+if rm.RIEMANN_ITER_LIMIT < iter_limit:
+    rm.RIEMANN_ITER_LIMIT = iter_limit  # int(height * 3)
 
 rm.precompute_coeffs()
 print("Done computing coefficients, iteration limit = " + str(rm.RIEMANN_ITER_LIMIT))
 
 # Input vector along critical line Re[s] = 0.5
-imag_part = np.arange(1, height, gap) * 1j
+imag_part = np.arange(imag_start, imag_end, imag_gap) * 1j
 s = imag_part + 0.5
 ax = np.imag(s)  # this is what will be used for x-axis of plot
 
 # Input vector along line Re[s] = 5
-s2 = imag_part + offset
+s2 = imag_part + real_offset
 
 # Make figure now so user doesn't have to stare at blank screen too much longer
 plt.figure()
@@ -77,11 +81,11 @@ plt.plot(ax, y, linewidth=1)
 show()
 
 # Calculate at line Re[s] = offset
-y2_log = np.log(np.pi) * (offset / 2) + rm.riemann_symmetric(s2, use_log=True, is_vertical=True) - np.real(loggamma(s2 / 2))
-y2 = np.real(np.exp(y2_log) / (ax * ax + offset * offset))
+y2_log = np.log(np.pi) * (real_offset / 2) + rm.riemann_symmetric(s2, use_log=True, is_vertical=True) - np.real(loggamma(s2 / 2))
+y2 = np.real(np.exp(y2_log) / (ax * ax + real_offset * real_offset))
 plt.plot(ax, -y2, linewidth=1)
 t3 = time.time()
-print("Plotted values along Re[s] = %1.1f in %1.2f seconds" % (offset, t3 - t2))
+print("Plotted values along Re[s] = %1.1f in %1.2f seconds" % (real_offset, t3 - t2))
 show()
 
 if __name__ == "__main__":

riemann_math.py
@@ -469,15 +469,11 @@ def riemann_row_non_negative(s,
         if is_vertical:  #
             # Use pre-computed power table, allowing 2D vectorization, and further speedup
             real_part = np.real(s[0])
-
-            # Create column vector of numbers 1, 2, ... RIEMANN_ITER_LIMIT
-#            bases = np.array([np.arange(1, RIEMANN_ITER_LIMIT + 1, 1, dtype=complex)]).T
             bases_real = np.power(bases, -real_part)
             NK2_real_power = np.multiply(NK2_array, bases_real.flatten())
 
             if USE_MATRIX_MULT:
                 len_s = len(s)
-#                powers = make_powers(s)
                 if powers.shape[1] == len_s:
                     cum_sum = np.dot(np.multiply(NK2_array, bases_real.flatten()), powers)
                 else:
@@ -485,7 +481,7 @@ def riemann_row_non_negative(s,
                     width = powers.shape[1]
                     cum_sum = np.empty(len_s, dtype=complex)
                     while startPoint <= len_s:
-                        ratios = np.power(bases, (imag_part[startPoint] - imag_part[0]) * 1j)
+                        ratios = np.power(bases, -(imag_part[startPoint] - imag_part[0]) * 1j)
                         NK2_power = np.multiply(NK2_real_power, ratios.flatten())
                         endPoint = startPoint + width
                         if endPoint <= len_s:
@@ -494,6 +490,7 @@ def riemann_row_non_negative(s,
                             cum_sum[startPoint:len_s] = np.dot(NK2_power, powers[:,0:(len_s - startPoint)])
                         startPoint += width
             else:
+                # Use slower method that requires lots of complex exponential calculations
                 # Replicate this column for each input value, creating a 2D array of size RIEMANN_ITER_LIMT x len(s)
                 bases2 = np.repeat(bases, len(s), axis=1)
                 # Raise all numbers in 2D array to each value in s. In matlab, we would do bsxfun(@power, bases, s)
@@ -569,7 +566,7 @@ def log_riemann(s, do_eta=False, is_vertical=False):
 def riemann_symmetric(s, use_log=False, is_vertical=False):
     if not use_log:
 
-        if np.mean(abs(s)) > 400:
+        if np.max(abs(s)) > 400:
             r = riemann_symmetric(s, use_log=True, is_vertical=is_vertical)
 
             # Compress magnitude range by 10-fold
@@ -612,6 +609,13 @@ def gamma_with_progress(s):
         row_count = 0
         if computation_progress_callback is not None:
             computation_progress_callback(np.size(s) * 50)
+
+    if np.max(np.abs(s)) >= 400:
+        r = loggamma(s)
+        r = r - np.real(r) * 0.9
+        return np.exp(r)
+
+        return
 
     # Call vectorized version
     return gamma(s)