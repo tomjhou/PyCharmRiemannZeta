dd5e94da223dd8947cc05481d6fca9df92dc5d38
Thomas Jhou <tomjhou@gmail.com>
2021-08-16 22:41:12 -0400

    Moved all code to do_main() so that program can be run from python console without needing reload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+from functools import partial
 import matplotlib as mpl
 mpl.use('TKAgg')
 
@@ -9,13 +10,15 @@ quit_flag = False
 # Gaps between various menu windows
 PADDING_PIXELS = 5
 is_android = False
+if 'ANDROID_BOOTLOGO' in environ:
+    is_android = True
 
 
 def do_vectors():
     import riemann_vectors
 
 
-def do_heatmap():
+def do_heatmap(root):
 
     win = tk.Toplevel(root)
     # Place just below earlier window
@@ -40,61 +43,67 @@ def do_critical_line():
     import plot_critical_line_values
 
 
-def do_exit():
+def do_exit(root):
     global quit_flag
     quit_flag = True
     root.quit()
 
 
-if 'ANDROID_BOOTLOGO' in environ:
-    is_android = True
+def do_main():
+    global quit_flag
 
-root = tk.Tk()
-root.title(string="Choose")
+    root = tk.Tk()
+    root.title(string="Choose")
 
-if is_android:
-    # Use simpler GUI on PyDroid, and larger buttons
-    frame1 = root
-    ipad = 20
-else:
-    frame1 = tk.Frame(root, highlightbackground="black", highlightthickness=1, relief="flat", borderwidth=5)
-    frame1.pack(side=tk.TOP, fill=tk.BOTH, padx=20, pady=20)
-    ipad = 10
+    if is_android:
+        # Use simpler GUI on PyDroid, and larger buttons
+        frame1 = root
+        ipad = 20
+    else:
+        frame1 = tk.Frame(root, highlightbackground="black", highlightthickness=1, relief="flat", borderwidth=5)
+        frame1.pack(side=tk.TOP, fill=tk.BOTH, padx=20, pady=20)
+        ipad = 10
 
-ttk.Label(frame1, text="Choose program").pack(fill=tk.X, pady=5)
-ttk.Button(frame1, text="Heatmaps", command=do_heatmap).pack(fill=tk.X,
-                                                             padx=10, pady=5,
-                                                             ipadx=ipad, ipady=ipad)
-ttk.Button(frame1, text="Critical line plot", command=do_critical_line).pack(fill=tk.X,
-                                                                             padx=10, pady=5,
-                                                                             ipadx=ipad, ipady=ipad)
-ttk.Button(frame1, text="Riemann vectors", command=do_vectors).pack(fill=tk.X,
-                                                                    padx=10, pady=5,
-                                                                    ipadx=ipad, ipady=ipad)
-ttk.Button(frame1, text="Exit", command=do_exit).pack(fill=tk.X,
-                                                      padx=10, pady=5,
-                                                      ipadx=ipad, ipady=ipad)
+    ttk.Label(frame1, text="Choose program").pack(fill=tk.X, pady=5)
+    ttk.Button(frame1, text="Heatmaps", command=partial(do_heatmap, root)).pack(fill=tk.X,
+                                                                 padx=10, pady=5,
+                                                                 ipadx=ipad, ipady=ipad)
+    ttk.Button(frame1, text="Critical line plot", command=do_critical_line).pack(fill=tk.X,
+                                                                                 padx=10, pady=5,
+                                                                                 ipadx=ipad, ipady=ipad)
+    ttk.Button(frame1, text="Riemann vectors", command=do_vectors).pack(fill=tk.X,
+                                                                        padx=10, pady=5,
+                                                                        ipadx=ipad, ipady=ipad)
+    ttk.Button(frame1, text="Exit", command=partial(do_exit, root)).pack(fill=tk.X,
+                                                          padx=10, pady=5,
+                                                          ipadx=ipad, ipady=ipad)
 
 
-if is_android:
-    print("Android!")
-    # On PyDroid, I can't get root.geometry() to do anything. So just skip it.
-else:
-    # Place in top left corner of screen
-    root.geometry("+%d+%d" % (PADDING_PIXELS, PADDING_PIXELS))
+    if is_android:
+        print("Android!")
+        # On PyDroid, I can't get root.geometry() to do anything. So just skip it.
+    else:
+        # Place in top left corner of screen
+        root.geometry("+%d+%d" % (PADDING_PIXELS, PADDING_PIXELS))
 
-# Force window to show, otherwise winfo_geometry() will return zero
-frame1.update()
-print("Created main window with geometry " + root.winfo_geometry())
+    # Force window to show, otherwise winfo_geometry() will return zero
+    frame1.update()
+    print("Created main window with geometry " + root.winfo_geometry())
 
-# frame1.tkraise()
+    # frame1.tkraise()
 
-# Because matplotlib.close() will terminate this loop, we wrap it in another loop
-while not quit_flag:
-    tk.mainloop()
-    print("Mainloop exited.")
-    if not quit_flag:
-        print(" Reentering now.")
+    # Clear flag in case we are running for the second time
+    quit_flag = False
 
-print("Finished")
-tk.destroy()
+    # Because matplotlib.close() will terminate this loop, we wrap it in another loop
+    while not quit_flag:
+        tk.mainloop()
+        print("Mainloop exited.")
+        if not quit_flag:
+            print(" Reentering now.")
+
+    print("Finished")
+    root.destroy()
+
+#if __name__ == '__main__':   #
+do_main()
